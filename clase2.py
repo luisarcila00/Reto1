5956a27bfc73d4cc80fb6cdd16b6ebc4e4a5abdd
luisarcila00 <luisarcila00@gmail.com>
2021-05-03 15:52:51 -0500

    first commit

---
+++ clase2.py
@@ -0,0 +1,14 @@
+#Reto 1
+numero_5 = 2+2+2-2/2
+numero_6 = 2*2+2*2/2
+numero_7 = 2+2+2+2/2
+numero_8 = 2*2*2*2/2
+numero_9 = 2*2*2+2/2
+numero_10 = 2*2+2+2+2
+
+print(numero_5)
+print(numero_6)
+print(numero_7)
+print(numero_8)
+print(numero_9)
+print(numero_10)
