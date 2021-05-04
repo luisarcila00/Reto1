dc3977508ca854206a05871c50a0e2d35bf874f7
luisarcila00 <luisarcila00@gmail.com>
2021-05-04 08:34:56 -0500

    a

---
+++ Clase 3/recibir_datos.py
@@ -0,0 +1,11 @@
+#Entradas
+PESO_KILO = 1000
+peso_empacado = input('Ingrese el peso en gramos empacado: ')
+precio_empacadao = input('Ingrese el precio empacado: ')
+peso_empacado = float(peso_empacado)
+precio_empacadao = float(precio_empacadao)
+precio_gramo = precio_empacadao / peso_empacado
+precio_kilo = precio_gramo * PESO_KILO
+print('El precio por kilo es:',precio_kilo)
+
+
