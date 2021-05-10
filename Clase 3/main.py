f5557e3c91929514ab66c1db75d4f7c909c0d529
andres <andresarcila10@gmail.com>
2021-05-09 21:03:33 -0500

    reto 1

---
+++ Clase 3/main.py
@@ -0,0 +1,4 @@
+import mis_retos as f
+print(f.simular_recaudo_producto(100,1000000,0)) 
+print(f.simular_recaudo_producto(1000,1000000,5)) 
+print(f.simular_recaudo_producto(158.53,2354837,5))
--- Clase 3/mis_retos.py
+++ Clase 3/mis_retos.py
@@ -9,3 +9,13 @@ def precio_por_kilo(peso_empacado,precio_empacado,peso_kilo = 1000):
     precio_gramo = precio_empacado / peso_empacado
     precio_kilo = precio_gramo * PESO_KILO
     return round(precio_kilo,2)
+
+def simular_recaudo_producto(precio_base, cantidad, porcentaje_actual=0):
+    porcentaje_actual = porcentaje_actual  + 100
+    precio_base_mas_iva = (precio_base * porcentaje_actual) / 100
+    precio_total_mas_iva = precio_base_mas_iva * cantidad
+    iva = 1.19
+    precio_mas_iva = (precio_base * iva) 
+    precio_total_iva_19 = (precio_mas_iva * cantidad) 
+    valor_recaudado = precio_total_iva_19 - precio_total_mas_iva
+    return round(valor_recaudado,2)
