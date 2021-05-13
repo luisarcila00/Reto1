da1c60e07e5289f3aae201896b8f14c9f30ef9a2
luisarcila00 <luisarcila00@gmail.com>
2021-05-13 10:27:56 -0500

    dd

---
+++ Clase 6/ejercicio_9.py
@@ -0,0 +1,19 @@
+def desperdicio_de_gaseosa():
+
+    pass
+
+lista = ['0',90,'hola','6']
+hola = lista
+print(hola[-1])
+
+print('Hola' * 3)
+k = 'Esto es una cadena. y esto?'
+print(k.capitalize()) # Pone mayuscula la primera 
+print(k.upper())
+print(k.lower())
+print(k.find('l'))
+print(k.find('l',3))
+
+parte1 = k.split('.')[1]
+print(parte1)
+k.strip() # quita los espacios hace lo que el trim
